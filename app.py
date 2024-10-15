52b8b413b13bf6eb67e43dffe0a21a76b694b3d7
xiaop1ng <jianchaoping@gmail.com>
2024-10-15 18:01:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/hello")
+def hello():
+    return {
+        "status": 200,
+        "message": "success",
+        "data": [
+            "hello", 
+            "world"
+        ]
+    }
